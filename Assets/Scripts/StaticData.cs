86c7007d2000d1b78a1792229e013f17158db2bd
Seb18456 <sebastian18678@gmail.com>
2023-10-07 19:22:25 -0400

    #1 - Finished Player Movement & started Robot Movement
    Player is fully functional, robot is in progress. Added static data in the StaticData script. Added slip physics. Added player and robot prefab.

--- Assets/Scripts/StaticData.cs
+++ Assets/Scripts/StaticData.cs
@@ -1,8 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
-public class StaticData : MonoBehaviour
+public static class StaticData
 {
     // Scenes
     public static int TutorialWorld = 0;
@@ -10,4 +9,9 @@ public class StaticData : MonoBehaviour
     public static int WorldOne = 1;
 
     public static int WorldTwo = 2;
+
+    // Movement Properties
+    public static Vector2 mouseSensitivity = new Vector2(40, 40) * 5; 
+
+    public static float xRotation, yRotation;
 }
