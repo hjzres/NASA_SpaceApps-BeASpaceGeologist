04950f782f3af01cb38d446a29495692770988e2
Andrei <142443853+hjzres@users.noreply.github.com>
2023-10-07 13:10:16 -0400

    Added the basic structure of the game

---
+++ Assets/Scripts/StaticData.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticData : MonoBehaviour
+{
+    // Scenes
+    public static int TutorialWorld = 0;
+
+    public static int WorldOne = 1;
+
+    public static int WorldTwo = 2;
+}
