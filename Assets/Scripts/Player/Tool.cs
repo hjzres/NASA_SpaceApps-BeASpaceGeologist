597a4660e3accccf6c99b2f864ee503e80ba0565
bellflwr <70229194+bellflwr@users.noreply.github.com>
2023-10-08 11:17:35 -0400

    Added interact script to player and robot prefabs, and created drill script.

--- Assets/Scripts/Player/Tool.cs
+++ Assets/Scripts/Player/Tool.cs
@@ -2,8 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tool : MonoBehaviour
-{
+public class Tool {
     public virtual void LeftClick(RaycastHit hit) {
         Debug.LogWarning("The tool might not have been assigned properly.");
     }
